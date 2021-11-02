b370a7e1f07dadc022521b304d4e186960d751df
iamgois <thiagogoisjunior@gmail.com>
2021-11-02 00:30:35 -0300

    Version 1.0

--- app.js
+++ app.js
@@ -3,16 +3,8 @@
         var pay = document.getElementById('pay').value;
         var rest = money - pay
 
-    if (rest <= 0) {
-        document.getElementById('resultado').innerHTML = "O valor que resta é: " + rest + ", você gasta mais do que ganha!"
-    } else if (rest <= 10/100 * money) {
-        document.getElementById('resultado').innerHTML = "O valor que resta é: " + rest + ", Sua saúde financeira é Pessima"
-    } else if (rest <= 20/100 * money) {
-        document.getElementById('resultado').innerHTML = "O valor que resta é: " + rest + ", Sua saúde financeira é Ruim!"
-    } else if (rest <= 40/100 * money) {
-        document.getElementById('resultado').innerHTML = "O valor que resta é: " + rest + ", Sua saúde financeira é Boa!"
-    } else if (rest <= 60/100 * money) {
-        document.getElementById('resultado').innerHTML = "O valor que resta é: " + rest + ", Sua saúde financeira é Excelente!"
+    if (rest >= 0) {
+        document.getElementById('resultado').innerHTML = "O valor que resta é: R$" + rest + ", agora podemos organizar!"
     }
 }
 
@@ -21,8 +13,8 @@ function valoresDivididos() {
         var pay = document.getElementById('pay').value;
         var rest = money - pay
 
-    document.getElementById('quarenta').innerHTML = 40/100 * rest;
-    document.getElementById('trinta').innerHTML = 30/100 * rest;
-    document.getElementById('vinte').innerHTML = 20/100 * rest;
-    document.getElementById('dez').innerHTML = 10/100 * rest;
+    document.getElementById('quarenta').innerHTML = 'R$' + 40/100 * rest;
+    document.getElementById('trinta').innerHTML = 'R$' +  30/100 * rest;
+    document.getElementById('vinte').innerHTML = 'R$' +  20/100 * rest;
+    document.getElementById('dez').innerHTML = 'R$' +  10/100 * rest;
 }
